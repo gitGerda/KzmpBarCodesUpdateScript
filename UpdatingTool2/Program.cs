ee3b6fa8bd7a7b2172819a7f380a2a391d625bc2
gitGerda <42517716+gitGerda@users.noreply.github.com>
2021-08-26 09:26:25 +0300

    260821_0926

--- UpdatingTool2/Program.cs
+++ UpdatingTool2/Program.cs
@@ -6,18 +6,48 @@ using System.Threading.Tasks;
 using V83;
 using System.Diagnostics;
 using System.Threading;
+using System.Runtime.InteropServices;
+
 
 
 namespace UpdatingTool
 {
     class Program
     {
+
+        [DllImport("user32.dll")]
+        public static extern int DeleteMenu(IntPtr hMenu, int nPosition, int wFlags);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+
+        private const int MF_BYCOMMAND = 0x00000000;
+        public const int SC_CLOSE = 0xF060;
+        public const int SC_MINIMIZE = 0xF020;
+        public const int SC_MAXIMIZE = 0xF030;
+        public const int SC_SIZE = 0xF000;
+
         public static bool timerFlag = true;
         public static DateTime UpdatingStart;
         public static int posX;
         public static int posY;
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            IntPtr handle = GetConsoleWindow();
+            IntPtr sysMenu = GetSystemMenu(handle, false);
+
+            if (handle != IntPtr.Zero)
+            {
+                DeleteMenu(sysMenu, SC_MINIMIZE, MF_BYCOMMAND);
+                DeleteMenu(sysMenu, SC_MAXIMIZE, MF_BYCOMMAND);
+                DeleteMenu(sysMenu, SC_SIZE, MF_BYCOMMAND);
+            }
+
             int mode = StartInformation();
 
             TimerCallback tm = new TimerCallback(TimerProcess);
@@ -36,7 +66,7 @@ namespace UpdatingTool
                 Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine("Connection attempt...");
 
-                Com1sObject = com1s.Connect("Srvr='ws-oit-007';Ref='NZP_local2';Usr='test';Pwd='852456';");
+                Com1sObject = com1s.Connect("Srvr='vs-nzp';Ref='nzp';Usr='BarCodesUploadUser';Pwd='852456';");
 
                 Console.ForegroundColor = ConsoleColor.Magenta;
                 Console.Write("@logs> ");
@@ -49,6 +79,11 @@ namespace UpdatingTool
                     Console.Write("@logs> ");
                     Console.ForegroundColor = ConsoleColor.Blue;
                     Console.WriteLine("NZP@Barcodes and SS-BD@kzmp_stickers_NiceLabel updating...");
+
+                    if (handle != IntPtr.Zero)
+                    {
+                        DeleteMenu(sysMenu, SC_CLOSE, MF_BYCOMMAND);
+                    }
 
                     posX = Console.CursorLeft;
                     posY = Console.CursorTop;
@@ -80,6 +115,11 @@ namespace UpdatingTool
 
                     UpdatingStart = DateTime.Now;
                     Timer _timer = new Timer(tm, null, 0, 1000);
+
+                    if (handle != IntPtr.Zero)
+                    {
+                        DeleteMenu(sysMenu, SC_CLOSE, MF_BYCOMMAND);
+                    }
 
                     Com1sObject.СинхронизацияДанных.СинхронизацияНоменклатур();
                     Com1sObject.СинхронизацияДанных.ОбновлениеБазыШтрихкодовMSSQL();
@@ -131,15 +171,14 @@ namespace UpdatingTool
             Com1sObject = null;
             com1s = null;
 
-            Environment.Exit(0);
-
+            Console.ReadKey();
         }
 
         public static int StartInformation()
         {
             Console.SetWindowSize(98, 40);
             Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine("*********************************DATABASE UPDATING TOOL v1.0****************************************");
+            Console.WriteLine("*********************************DATABASE UPDATING TOOL v1.0**************************************");
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine("░░░████▌█████▌█░████████▐▀██▀\t" + "░░░████▌█████▌█░████████▐▀██▀\t" + "░░░████▌█████▌█░████████▐▀██▀\n" +
@@ -154,12 +193,12 @@ namespace UpdatingTool
                           "░░░░░░█▀██▄▄▄░▄▄▀▀▒█▀█░\t\t" + "░░░░░░█▀██▄▄▄░▄▄▀▀▒█▀█░\t\t" + "░░░░░░█▀██▄▄▄░▄▄▀▀▒█▀█░\n");
 
             Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine("*********************************DATABASE UPDATING TOOL v1.0****************************************");
+            Console.WriteLine("*********************************DATABASE UPDATING TOOL v1.0**************************************");
             Console.WriteLine("          -----------------------------------------------------------------------------");
             Console.WriteLine("         |                                #INFO#                                       |");
             Console.WriteLine("         | * Press 1 to update NZP@Barcodes and SS-BD@kzmp_stickers_NiceLabel          |");
-            Console.WriteLine("         | * Press 2 to update NZP@Nomenclature and SS-BD@kzmp_stickers_NiceLabel (Warn|");
-            Console.WriteLine("         |   ing: high load on the system )                                            |");
+            Console.WriteLine("         | * Press 2 to update NZP@Nomenclature and SS-BD@kzmp_stickers_NiceLabel      |");
+            Console.WriteLine("         |   (Warning: mode number 2 causes a heavy load on the system )               |");
             Console.WriteLine("         | * Press 0 to exit                                                           |");
             Console.WriteLine("         |                                                                             |");
             Console.WriteLine("          ----------------------------------------------------------------------------");
@@ -203,9 +242,9 @@ namespace UpdatingTool
 
         public static void TimerProcess(object obj)
         {
+            Console.SetCursorPosition(posX, posY);
             for (int i = 0; i < 60; i++)
             {
-                Console.SetCursorPosition(posX, posY);
                 Console.Write("");
             }
 
@@ -216,7 +255,6 @@ namespace UpdatingTool
             Console.Write("@logs> ");
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.Write("Timer of the update process: " + Convert.ToString(diff1));
-
         }
     }
 }
